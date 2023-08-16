171322fd22d75912167465cd30e657c7a785a1d4
Tyler Twining <twijn@twijn.net>
2023-08-15 22:31:09 -0400

    Add commas to seconds in timeouts

--- twitch/listeners/globalTimeoutLog.js
+++ twitch/listeners/globalTimeoutLog.js
@@ -4,7 +4,7 @@ const listener = {
     name: "globalTimeoutLog",
     eventName: "timeout",
     listener: async (client, streamer, chatter, duration, timeto, userstate) => {
-        utils.Discord.addTimeout(`${utils.parseDate(Date.now())} [#${streamer.login}] ${chatter.display_name} timed out for ${duration} second${duration === 1 ? "" : "s"}`);
+        utils.Discord.addTimeout(`${utils.parseDate(Date.now())} [#${streamer.login}] ${chatter.display_name} timed out for ${utils.comma(duration)} second${duration === 1 ? "" : "s"}`);
     }
 };
 
