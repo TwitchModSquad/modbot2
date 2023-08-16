4906864fd10580a1d843e20de5ba9aea7d0c6bc2
Tyler Twining <twijn@twijn.net>
2023-08-15 22:23:40 -0400

    Post messages on global bans & timeouts

--- discord/modbot/listeners/readyMessage.js
+++ discord/modbot/listeners/readyMessage.js
@@ -1,5 +1,7 @@
 const client = global.client.modbot;
 
+const utils = require("../../../utils/");
+
 const listener = {
     name: 'readyMessage',
     eventName: 'ready',
@@ -7,6 +9,7 @@ const listener = {
     listener () {
         console.log(`[MB] Discord bot ready! Logged in as ${client.user.tag}!`);
         console.log(`[MB] Bot has started with ${client.users.cache.size} users, in ${client.channels.cache.size} channels of ${client.guilds.cache.size} guilds.`);
+        utils.Discord.init().catch(console.error);
     }
 };
 
--- utils/discord/index.js
+++ utils/discord/index.js
@@ -1,9 +1,12 @@
+const { TextChannel, Message, codeBlock } = require("discord.js");
+
+const config = require("../../config.json");
+
+const DiscordMessage = require("./DiscordMessage");
 const DiscordUser = require("./DiscordUser");
 const DiscordUserSchema = require("./DiscordUserSchema");
 
 const Cache = require("../Cache/Cache");
-
-const client = require("../../discord/modbot/");
 
 class Discord {
 
@@ -14,12 +17,48 @@ class Discord {
     userCache = new Cache(1 * 60 * 60 * 1000); // 1 hour cache
 
     /**
+     * Various Discord channels
+     * @type {{ban:TextChannel,live:TextChannel}}
+     */
+    channels = {
+        ban: null,
+        live: null,
+    }
+
+    /**
+     * Various Discord messages
+     * @type {{globalTimeout:Message,globalBan:Message}}
+     */
+    messages = {
+        globalTimeout: null,
+        globalBan: null,
+    };
+
+    /**
+     * Records if message content has changed
+     * @type {boolean}
+     */
+    messageChanges = false;
+
+    /**
+     * Carries cached timeout content
+     * @type {string}
+     */
+    timeoutContent = "";
+
+    /**
+     * Carries cached ban content
+     * @type {string}
+     */
+    banContent = "";
+
+    /**
      * Internal method for retrieving a user if it is not present in the database
      * @param {string} id 
      */
     getUserByIdByForce(id) {
         return new Promise((resolve, reject) => {
-            client.users.fetch(id).then(async user => {
+            global.client.modbot.users.fetch(id).then(async user => {
                 const discordUser = await DiscordUser.create({
                     _id: user.id,
                     globalName: user.globalName,
@@ -53,6 +92,151 @@ class Discord {
                 }
             }
         }, overrideCache);
+    }
+
+    /**
+     * Creates a new Global Timeout message
+     * @returns {Promise<Message>}
+     */
+    async createGlobalTimeoutMessage() {
+        await DiscordMessage.deleteMany({twitchGlobalTimeouts: true});
+
+        const message = await this.channels.ban.send("# Global Timeouts\n" + codeBlock("None to display!"));
+        await DiscordMessage.create({
+            _id: message.id,
+            twitchGlobalTimeouts: true,
+        });
+        this.messages.globalTimeout = message;
+        try {
+            await message.pin("Pinning global message");
+        } catch(e) {
+            console.error(e);
+        }
+        return message;
+    }
+
+    /**
+     * Creates a new Global Ban message
+     * @returns {Promise<Message>}
+     */
+    async createGlobalBanMessage() {
+        await DiscordMessage.deleteMany({twitchGlobalBans: true});
+
+        const message = await this.channels.ban.send("# Global Bans\n" + codeBlock("None to display!"));
+        await DiscordMessage.create({
+            _id: message.id,
+            twitchGlobalBans: true,
+        });
+        this.messages.globalBan = message;
+        try {
+            await message.pin("Pinning global message");
+        } catch(e) {
+            console.error(e);
+        }
+        return message;
+    }
+
+    /**
+     * Adds a new timeout to the global timeouts
+     * @param {string} timeoutMessage
+     * @returns {Promise<Message>}
+     */
+    addTimeout(timeoutMessage) {
+        const splitMessage = this.timeoutContent.replace("`", "").split("\n");
+        let effectiveLines = timeoutMessage;
+        splitMessage.forEach(line => {
+            if (effectiveLines.length >= 1750) return;
+            if (line.startsWith("#")) return;
+            if (line.length < 4) return;
+            if (line === "None to display!") return;
+
+            effectiveLines += `\n${line}`;
+        });
+        this.timeoutContent = effectiveLines;
+        this.messageChanges = true;
+    }
+
+    /**
+     * Adds a new ban to the global bans
+     * @param {string} timeoutMessage
+     * @returns {Promise<Message>}
+     */
+    addBan(banMessage) {
+        const splitMessage = this.banContent.replace("`", "").split("\n");
+        let effectiveLines = banMessage;
+        splitMessage.forEach(line => {
+            if (effectiveLines.length >= 1750) return;
+            if (line.startsWith("#")) return;
+            if (line.length < 4) return;
+            if (line === "None to display!") return;
+
+            effectiveLines += `\n${line}`;
+        });
+        this.banContent = effectiveLines;
+        this.messageChanges = true;
+    }
+
+    async updateMessages() {
+        if (!this.messageChanges) return;
+        
+        if (!this.timeoutContent.includes("`"))
+            await this.messages.globalTimeout.edit({
+                content: `# Global Timeouts\n${codeBlock(this.timeoutContent)}`,
+            });
+        if (!this.banContent.includes("`"))
+            await this.messages.globalBan.edit({
+                content: `# Global Bans\n${codeBlock(this.banContent)}`,
+            });
+        this.messageChanges = false;
+    }
+
+    /**
+     * Initializes Discord-related services
+     * @returns {Promise<null>}
+     */
+    async init() {
+        this.channels.ban = await global.client.modbot.channels.fetch(config.discord.modbot.channels.ban);
+        this.channels.live = await global.client.modbot.channels.fetch(config.discord.modbot.channels.live);
+
+        const globalTimeoutMessage = await DiscordMessage.find({twitchGlobalTimeouts: true})
+                .sort({time_sent: -1})
+                .limit(1);
+        const globalBanMessage = await DiscordMessage.find({twitchGlobalBans: true})
+                .sort({time_sent: -1})
+                .limit(1);
+
+        if (globalTimeoutMessage.length > 0) {
+            try {
+                this.messages.globalTimeout = await this.channels.ban.messages.fetch(globalTimeoutMessage[0]._id);
+            } catch(e) {
+                console.error(e);
+            }
+        }
+
+        if (globalBanMessage.length > 0) {
+            try {
+                this.messages.globalBan = await this.channels.ban.messages.fetch(globalBanMessage[0]._id);
+            } catch(e) {
+                console.error(e);
+            }
+        }
+
+        if (!this.messages.globalTimeout)
+            await this.createGlobalTimeoutMessage();
+        if (!this.messages.globalBan)
+            await this.createGlobalBanMessage();
+
+        this.timeoutContent = this.messages.globalTimeout.content;
+        this.banContent = this.messages.globalBan.content;
+
+        console.log(
+            `[MB] Using channel #${this.channels.ban.name} for bans, #${this.channels.live.name} for livestreams\n` +
+            `[MB] Using message ${this.messages.globalTimeout.id} for timeouts, ${this.messages.globalBan.id} for bans`
+        );
+
+        setInterval(() => {
+            this.updateMessages();
+        }, 5000);
     }
 
 }
