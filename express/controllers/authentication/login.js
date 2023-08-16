31a46df4822233dbac70dbe3d54201ff6e83d99d
Tyler Twining <twijn@twijn.net>
2023-08-16 02:18:39 -0400

    Add login endpoint

---
+++ express/controllers/authentication/login.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const router = express.Router();
+
+const utils = require("../../../utils/");
+
+router.get("/", async (req, res) => {
+    const { session } = req;
+
+    if (!session) {
+        res.send("Session has not been initialized. Please reload the page");
+        return;
+    }
+
+    let twitchUsers = [];
+    let discordUsers = [];
+
+    let streamers = [];
+
+    if (req.session.identity) {
+        twitchUsers = await req.session.identity.getTwitchUsers();
+        discordUsers = await req.session.identity.getDiscordUsers();
+    
+        streamers = await req.session.identity.getStreamers();
+    }
+
+    res.render("panel/pages/authentication/login", {
+        twitchURI: utils.Authentication.Twitch.getURL("user:read:email moderator:manage:banned_users"),
+        discordURI: utils.Authentication.Discord.getURL(),
+        twitchUsers: twitchUsers,
+        discordUsers: discordUsers,
+        streamers: streamers,
+        comma: utils.comma,
+    });
+});
+
+module.exports = router;
