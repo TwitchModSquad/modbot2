686e06ff11561156af90507a1bfea8db01406643
Tyler Twining <twijn@twijn.net>
2023-08-15 22:23:52 -0400

    Add getStreamers method to identities

--- utils/Identity.js
+++ utils/Identity.js
@@ -23,4 +23,19 @@ identitySchema.methods.getDiscordUsers = async function () {
     return await global.utils.Schemas.DiscordUser.find({identity: this._id});
 }
 
+identitySchema.methods.getStreamers = async function () {
+    let streamerRoles = [];
+    const twitchUsers = await this.getTwitchUsers();
+    for (let i = 0; i < twitchUsers.length; i++) {
+        const roles = await twitchUsers[i].getStreamers();
+        for (let r = 0; r < roles.length; r++) {
+            const role = roles[r];
+            if (!streamerRoles.find(x => x.streamer.id === role.streamer.id)) {
+                streamerRoles.push(role);
+            }
+        }
+    }
+    return streamerRoles;
+}
+
 module.exports = mongoose.model("Identity", identitySchema);
