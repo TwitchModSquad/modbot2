2dde1ebc011cba902975af56888c61e1156eeff9
Tyler Twining <twijn@twijn.net>
2023-08-20 19:31:58 -0400

    Add authentication to main Express controller

--- express/controllers/index.js
+++ express/controllers/index.js
@@ -1,12 +1,39 @@
 const express = require("express");
 const router = express.Router();
 
+const utils = require("../../utils/");
+
+const api = require("./api/");
+
 const authentication = require("./authentication/");
 
 const panel = require("./panel/");
 const public = require("./public/");
 
 router.use("/auth", authentication);
+
+router.use(async (req, res, next) => {
+    const {cookies} = req;
+
+    const redirect = () => {
+        res.redirect("/auth/login");
+    }
+
+    if (cookies?.session) {
+        const session = await utils.Schemas.Session.findById(cookies.session)
+                .populate("identity");
+        if (session?.identity?.authenticated) {
+            req.session = session;
+            next();
+        } else {
+            redirect();
+        }
+    } else {
+        redirect();
+    }
+});
+
+router.use("/api", api);
 
 router.use("/", public);
 
