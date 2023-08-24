ab3265738de4a135d72f5167ce809c933ce25cac
Tyler Twining <twijn@twijn.net>
2023-08-24 16:29:14 -0400

    Add redirect for homepage

--- express/controllers/public/index.js
+++ express/controllers/public/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) =>  {
-    res.render("public/pages/index");
+    res.redirect("https://tms.to/");
 })
 
 module.exports = router;
