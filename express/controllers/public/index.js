17825d0b2d8d1065fef310ea21b783044215b709
Tyler Twining <twijn@twijn.net>
2023-09-01 23:06:04 -0400

    Update public redirect

--- express/controllers/public/index.js
+++ express/controllers/public/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) =>  {
-    res.redirect("https://tms.to/");
+    res.redirect("https://tms.to/auth/login");
 })
 
 module.exports = router;
