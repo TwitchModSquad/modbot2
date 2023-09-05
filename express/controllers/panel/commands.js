2422e8fe769bf89d236cfda1895af00bf34a2637
Tyler Twining <twijn@twijn.net>
2023-09-04 21:37:10 -0400

    Implement twitch command management

--- express/controllers/panel/commands.js
+++ express/controllers/panel/commands.js
@@ -7,10 +7,15 @@ const config = require("../../../config.json");
 
 const client = require("../../../discord/mbm/");
 
-const commands = [
+const discordCommands = [
     {label: "Chatdump", name: "chatdump"},
     {label: "User", name: "user"},
-]
+];
+const twitchCommands = [
+    {label: "Group", name: "group"},
+    {label: "Stats", name: "stats"},
+    {label: "TMS Stats", name: "tmsstats"},
+];
 
 router.get("/discord", async (req, res) => {
     const discordUsers = await req.session.identity.getDiscordUsers();
@@ -20,7 +25,36 @@ router.get("/discord", async (req, res) => {
         const guild = await utils.Schemas.DiscordGuild.findById(g.id);
         if (guild) guilds.push(guild);
     }
-    res.render("panel/pages/commands/discord", {guilds: guilds, commands});
+    res.render("panel/pages/commands/discord", {guilds: guilds, commands: discordCommands});
+});
+
+router.get("/twitch", async (req, res) => {
+    let users = await req.session.identity.getTwitchUsers();
+
+    let userLength = users.length;
+    for (let i = 0; i < userLength; i++) {
+        users = [
+            ...users,
+            ...(await users[i].getStreamers()).map(x => x.streamer),
+        ];
+    }
+
+    res.render("panel/pages/commands/twitch", {users: users, commands: twitchCommands, comma: utils.comma});
+});
+
+router.get("/twitch/:id", async (req, res) => {
+    let users = await req.session.identity.getTwitchUsers();
+
+    let userLength = users.length;
+    for (let i = 0; i < userLength; i++) {
+        users = [
+            ...users,
+            ...(await users[i].getStreamers()).map(x => x.streamer),
+        ];
+    }
+
+    const user = users.find(x => x._id === req.params.id);
+    res.render("panel/pages/commands/twitch", {users: users, user: user, commands: twitchCommands, comma: utils.comma});
 });
 
 router.get("/discord/:id", async (req, res) => {
@@ -32,7 +66,7 @@ router.get("/discord/:id", async (req, res) => {
         if (guild) guilds.push(guild);
     }
     let guild = guilds.find(x => x._id === req.params.id);
-    res.render("panel/pages/commands/discord", {guilds: guilds, guild: guild, commands});
+    res.render("panel/pages/commands/discord", {guilds: guilds, guild: guild, commands: discordCommands});
 });
 
 router.use(bodyParser.urlencoded({extended: true}));
@@ -55,12 +89,41 @@ router.post("/discord/:id", async (req, res) => {
 
     if (!guild.commands) guild.commands = {};
 
-    commands.forEach(cmd => {
+    discordCommands.forEach(cmd => {
         guild.commands[cmd.name] = req.body.hasOwnProperty(cmd.name);
     });
 
     await guild.save();
     res.redirect(`/panel/commands/discord/${req.params.id}`)
+});
+
+router.post("/twitch/:id", async (req, res) => {
+    const error = err => {
+        res.redirect(`/panel/commands/twitch/${req.params.id}?error=${encodeURIComponent(err)}`)
+    }
+
+    let users = await req.session.identity.getTwitchUsers();
+
+    let userLength = users.length;
+    for (let i = 0; i < userLength; i++) {
+        users = [
+            ...users,
+            ...(await users[i].getStreamers()).map(x => x.streamer),
+        ];
+    }
+
+    const user = users.find(x => x._id === req.params.id);
+    if (!user)
+        return error("User with the provided ID was not found!");
+
+    if (!user.commands) user.commands = {};
+
+    twitchCommands.forEach(cmd => {
+        user.commands[cmd.name] = req.body.hasOwnProperty(cmd.name);
+    });
+
+    await user.save();
+    res.redirect(`/panel/commands/twitch/${req.params.id}`)
 });
 
 module.exports = router;
