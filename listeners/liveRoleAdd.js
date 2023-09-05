26060e3de7765cd1ea6ad288f07d6ce1db630314
Tyler Twining <twijn@twijn.net>
2023-09-04 23:30:53 -0400

    Implement live now role to TMS/TLMS

---
+++ listeners/liveRoleAdd.js
@@ -0,0 +1,30 @@
+const { HelixStream } = require("twitch")
+
+const utils = require("../utils/");
+const config = require("../config.json");
+
+const listener = {
+    event: "member_live",
+    /**
+     * @param {*} user 
+     * @param {HelixStream} stream 
+     * @param {*} activity
+     */
+    func: async (user, stream, activity) => {
+        const retrievedUser = await utils.Twitch.getUserById(user._id);
+        if (retrievedUser?.identity) {
+            const discordUsers = await retrievedUser.identity.getDiscordUsers();
+            for (let i = 0; i < discordUsers.length; i++) {
+                const user = discordUsers[i];
+                utils.Discord.guilds.tms.members.fetch(user._id).then(member => {
+                    member.roles.add(config.discord.tms.roles.live).catch(console.error);
+                }, () => {});
+                utils.Discord.guilds.tlms.members.fetch(user._id).then(member => {
+                    member.roles.add(config.discord.tlms.roles.live).catch(console.error);
+                }, () => {});
+            }
+        }
+    }
+}
+
+module.exports = listener;
---
+++ listeners/liveRoleRemove.js
@@ -0,0 +1,29 @@
+const { Message } = require("discord.js");
+
+const utils = require("../utils/");
+const config = require("../config.json");
+
+const listener = {
+    event: "member_live_offline",
+    /**
+     * @param {*} livestream
+     * @param {Message} message
+     */
+    func: async (livestream, message) => {
+        const retrievedUser = await utils.Twitch.getUserById(user._id);
+        if (retrievedUser?.identity) {
+            const discordUsers = await retrievedUser.identity.getDiscordUsers();
+            for (let i = 0; i < discordUsers.length; i++) {
+                const user = discordUsers[i];
+                utils.Discord.guilds.tms.members.fetch(user._id).then(member => {
+                    member.roles.remove(config.discord.tms.roles.live).catch(console.error);
+                }, () => {});
+                utils.Discord.guilds.tlms.members.fetch(user._id).then(member => {
+                    member.roles.remove(config.discord.tlms.roles.live).catch(console.error);
+                }, () => {});
+            }
+        }
+    }
+}
+
+module.exports = listener;
