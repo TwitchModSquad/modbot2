6f36d8dd2dff2c352f3e1b2ce5752f6e63c7aee4
Tyler Twining <twijn@twijn.net>
2023-09-05 20:17:57 -0400

    Add twitch chat announcement when TMS is live

---
+++ intervals/twitchChatAnnouncement.js
@@ -0,0 +1,35 @@
+const utils = require("../utils/");
+const config = require("../config.json");
+
+const randomItem = items => {
+    return items[Math.floor(Math.random()*items.length)];
+}
+
+let nextIndex = 0;
+const interval = {
+    interval: 60000,
+    onStartup: true,
+    run: async () => {
+        if (!utils.StatsManager.getMemberStreams().find(x => x.user.id !== config.twitch.id))
+            return;
+
+        let message = config.twitch.messages[nextIndex];
+
+        if (message === "random-streamer-shoutout") {
+            const streams = utils.StatsManager.getMemberStreams();
+            if (streams.length <= 1) return;
+            let stream;
+            while (!stream || stream.user.id === config.twitch.id) {
+                stream = randomItem(streams);
+            }
+            message = `Visit one of our members ${stream.user.display_name} playing ${stream.game.name} at twitch.tv/${stream.user.login} !`;
+        }
+
+        global.client.listen.member.client.say(config.twitch.username, message).catch(console.error);
+
+        nextIndex++;
+        if (nextIndex >= config.twitch.messages.length) nextIndex = 0;
+    },
+};
+
+module.exports = interval;
