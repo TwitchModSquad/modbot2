05c507288d675d9fee5cbe453c11edd00dd92110
Tyler Twining <twijn@twijn.net>
2024-01-23 15:28:25 -0500

    Replace intermittent logs with PM2 metrics

---
+++ intervals/updateStatus.js
@@ -0,0 +1,32 @@
+const utils = require("../utils/");
+
+const listenClients = require("../twitch/");
+
+const io = require("@pm2/io");
+
+const memberChannels = io.metric({
+    id: "app/realtime/memberChannels",
+    name: "Member Channels",
+});
+
+const cachedTwitch = io.metric({
+    id: "app/realtime/cached/twitch",
+    name: "Cached Twitch Users",
+});
+const cachedDiscord = io.metric({
+    id: "app/realtime/cached/discord",
+    name: "Cached Discord Users",
+});
+
+const interval = {
+    interval: 15000,
+    onStartup: true,
+    run: async () => {
+        memberChannels.set(listenClients.member.channels.length);
+
+        cachedTwitch.set(Object.keys(utils.Twitch.userCache.objectStore).length);
+        cachedDiscord.set(Object.keys(utils.Discord.userCache.objectStore).length);
+    },
+};
+
+module.exports = interval;
