67112f51e01bcc18540ceeddf93943190498046b
Tyler Twining <twijn@twijn.net>
2024-01-27 17:40:09 -0500

    Add authentication to /api endpoint

--- express/controllers/api/index.js
+++ express/controllers/api/index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
+const utils = require("../../../utils");
+
 const user = require("./user");
 const twitch = require("./twitch/");
+const me = require("./me");
 
 const io = require("@pm2/io");
 
@@ -16,8 +19,31 @@ router.use((r,_r,next) => {
     next();
 })
 
+router.use(async (req, res, next) => {
+    let session = req.headers.authorization;
+    if (!session) {
+        session = req.cookies?.session;
+    }
+
+    if (!session) {
+        res.status(401);
+        return res.json({ok: false, error: "Unauthorized"})
+    }
+
+    try {
+        req.session = await utils.SessionStore.getSessionById(session);
+        res.header("Access-Control-Allow-Origin: tms.to")
+        next();
+    } catch(err) {
+        res.status(401);
+        return res.json({ok: false, error: "Unauthorized"})
+    }
+});
+
 router.use("/user", user);
 
 router.use("/twitch", twitch);
+
+router.use("/me", me);
 
 module.exports = router;
